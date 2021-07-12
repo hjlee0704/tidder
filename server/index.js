3edc64447ec3d987667be0cc6868e4bb0e0535f8
Anthony Lee <ahjlee74@gmail.com>
2021-07-11 18:14:24 -0700

    ci/cd with bash scripts

--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const path = require('path');
-// const compression = require('compression');
 const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
 const PUBLIC_DIR = path.resolve(__dirname, '..', 'client', 'dist');
-//const router = require('./router');
+const router = require('./router');
 // app.use(compression());
 
 app.use(express.json());
@@ -30,29 +29,21 @@ app.get('/api/reddit/hot', (req, res) => {
       axios.get(`http://www.reddit.com${link}.json?limit=3`)
         .then(commentIndex => {
           let count = [];
-          // console.log(commentIndex.data[1].data.children);
           commentIndex.data[1].data.children.map((comment) => {
             if (typeof comment.data.body === 'undefined' || comment.data.body.includes('Join Our Discord Server') || comment.data.body.includes('Hello! This is just a quick reminder for')) {
-              //console.log('undefined');
               count.push(0);
             } else if (comment.data.body.includes('I am a bot') || comment.data.body.includes('Hello and welcome to') || comment.data.body.includes('The following alternative links are available')) {
-              //console.log('pass');
               count.push(1);
             } else if (comment.data.body.includes('[removed]')) {
-              //console.log('pass');
               count.push(2);
             } else {
               singlePost.comments.push(comment.data.body);
               count.push(3);;
             }
-            // console.log(count);
           })
           redditPosts.push(singlePost);
-          // console.log(count);
-           
           if(redditPosts.length === 50 && commentIndex.data[1].data.children[2]) {
             redditPosts.sort((a, b) => parseFloat(a.index) - parseFloat(b.index));
-            // console.log(count);
             return res.send(redditPosts);
           }
         })
@@ -119,37 +110,27 @@ app.get('/api/reddit/top', (req, res) => {
       axios.get(`http://www.reddit.com${link}.json?limit=3`)
         .then(commentIndex => {
           let count = [];
-          // console.log(commentIndex.data[1].data.children);
           commentIndex.data[1].data.children.map((comment) => {
             if (typeof comment.data.body === 'undefined' || comment.data.body.includes('Join Our Discord Server') || comment.data.body.includes('Hello! This is just a quick reminder for')) {
-              //console.log('undefined');
               count.push(0);
             } else if (comment.data.body.includes('I am a bot') || comment.data.body.includes('Hello and welcome to') || comment.data.body.includes('The following alternative links are available')) {
-              //console.log('pass');
               count.push(1);
             } else if (comment.data.body.includes('[removed]')) {
-              //console.log('pass');
               count.push(2);
             } else {
               singlePost.comments.push(comment.data.body);
               count.push(3);;
             }
-            // console.log(count);
           })
           redditPosts.push(singlePost);
-          // console.log(count);
-           
+ 
           if(redditPosts.length === 25 && commentIndex.data[1].data.children[2]) {
             redditPosts.sort((a, b) => parseFloat(a.index) - parseFloat(b.index));
-            // console.log(count);
             return res.send(redditPosts);
           }
         })
       }
 
-      // res.send(redditPosts);
-      // console.log(`http://www.reddit.com${link}.json`); comments.data[1].data.children[0].data.body
-      // res.send(`http://www.reddit.com${link}.json`);
     })
   .catch(error => {
     console.log(`faile due to ${error}`)
